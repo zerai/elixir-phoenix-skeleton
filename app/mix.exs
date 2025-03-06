60c7322c72c8dcf1e8cadbfb554fb324d957a796
zero <teclaizerai@gmail.com>
2025-03-06 19:10:52 +0100

    Tool: added 'credo' for static analysis
    - install credo
    - update CI, commit-stage wworkflow

--- app/mix.exs
+++ app/mix.exs
@@ -57,7 +57,8 @@ defmodule App.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
