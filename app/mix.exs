12b150a98b12d377551eff6042d670e3c54eb75f
zero <teclaizerai@gmail.com>
2025-03-07 15:09:29 +0100

    Tool: mix audit
    - install mix audit
    - setup CI job in commit-stage workflow

--- app/mix.exs
+++ app/mix.exs
@@ -58,7 +58,8 @@ defmodule App.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 
